54df932e0e265c9b8d3cbd18acc6d3ac49f0dcb1
Konstantin Krupovich <kkrupovich@gmail.com>
2022-08-02 14:06:37 +0300

    satellite attachment refactoring

--- Assets/Scripts/OrbiterView.cs
+++ Assets/Scripts/OrbiterView.cs
@@ -3,5 +3,39 @@ using UnityEngine;
 
 public class OrbiterView : NetworkBehaviour
 {
+    [Networked(OnChanged = nameof(AnchorSidesRefChanged))]
+    private NetworkId anchorRef { get; set; }
+    
+    [Networked(OnChanged = nameof(AnchorSidesRefChanged))]
+    private NetworkId satelliteRef { get; set; }
 
+
+    public void InitSatelliteForAnchorRef(NetworkId anchorRef)
+    {
+        this.anchorRef = anchorRef;
+        satelliteRef = Object.Id;
+    }
+
+    protected static void AnchorSidesRefChanged(Changed<OrbiterView> changed)
+    {
+        var curAncorRef = changed.Behaviour.anchorRef;
+        var curSatelliteRef = changed.Behaviour.satelliteRef;
+
+        if (curAncorRef && curSatelliteRef)
+            changed.Behaviour.AttachSatellite(curAncorRef, curSatelliteRef);
+    }
+
+    private void AttachSatellite(NetworkId curAncorRef, NetworkId curSatelliteRef)
+    {
+        Debug.LogFormat("AttachSatellite: {0} to: {1}", curSatelliteRef, curAncorRef);
+
+        var playerSatAnchorNTA = Runner.TryGetNetworkedBehaviourFromNetworkedObjectRef<NetworkTransformAnchor>(curAncorRef);
+        var orbiterNTA = Runner.TryGetNetworkedBehaviourFromNetworkedObjectRef<NetworkTransformAnchor>(curSatelliteRef);
+        if (playerSatAnchorNTA && orbiterNTA)
+        {
+            Debug.LogFormat("AttachedSatellite: {0} to: {1}", curSatelliteRef, curAncorRef);
+            orbiterNTA.transform.SetParent(playerSatAnchorNTA.transform);
+            orbiterNTA.transform.SetPositionAndRotation(playerSatAnchorNTA.transform.position, playerSatAnchorNTA.transform.rotation);
+        }
+    }
 }
--- Assets/Scripts/PlayerSatelliteVisual.cs
+++ Assets/Scripts/PlayerSatelliteVisual.cs
@@ -10,41 +10,11 @@ public class PlayerSatelliteVisual : NetworkBehaviour
 
     public NetworkTransformAnchor AnchorPosition => anchorPosition;
 
-    [Networked (OnChanged = nameof(AnchorSidesRefChanged)), HideInInspector] public NetworkId anchorRef { get; set; }
-    [Networked (OnChanged = nameof(AnchorSidesRefChanged)), HideInInspector] public NetworkId satelliteRef { get; set; }
-
-    protected static void AnchorSidesRefChanged(Changed<PlayerSatelliteVisual> changed)
-    {
-        var curAncorRef = changed.Behaviour.anchorRef;
-        var curSatelliteRef = changed.Behaviour.satelliteRef;
-
-        if (curAncorRef && curSatelliteRef)
-            changed.Behaviour.AttachSatellite(curAncorRef, curSatelliteRef);
-    }
-
-    private void AttachSatellite(NetworkId curAncorRef, NetworkId curSatelliteRef)
-    {
-        Debug.LogFormat("AttachSatellite: {0} to: {1}", curSatelliteRef, curAncorRef);
-
-        var playerSatAnchorNTA = Runner.TryGetNetworkedBehaviourFromNetworkedObjectRef<NetworkTransformAnchor>(curAncorRef);
-        var orbiterNTA = Runner.TryGetNetworkedBehaviourFromNetworkedObjectRef<NetworkTransformAnchor>(curSatelliteRef);
-        if (playerSatAnchorNTA && orbiterNTA)
-        {
-            Debug.LogFormat("AttachedSatellite: {0} to: {1}", curSatelliteRef, curAncorRef);
-            orbiterNTA.transform.parent = playerSatAnchorNTA.transform;
-            orbiterNTA.transform.SetPositionAndRotation(playerSatAnchorNTA.transform.position, playerSatAnchorNTA.transform.rotation);
-        }
-    }
+    
 
     public override void Spawned()
     {
-        Runner.Spawn(satellitePrefab, anchorPosition.transform.position, Quaternion.identity, Object.InputAuthority, InitSatellite);
+        Runner.Spawn(satellitePrefab, anchorPosition.transform.position, Quaternion.identity, Object.InputAuthority, 
+            (runner, obj) => obj.GetComponent<OrbiterView>().InitSatelliteForAnchorRef(Object.Id));
     }
-
-    private void InitSatellite(NetworkRunner runner, NetworkObject obj)
-    {
-        anchorRef = Object.Id;
-        satelliteRef = obj.Id;
-    }
-
 }
