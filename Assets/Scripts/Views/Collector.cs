ae09a5df7dac5dde43e27a5961fd1870f99b2204
Konstantin Krupovich <kkrupovich@gmail.com>
2022-08-13 22:34:16 +0300

    droppable proto

--- Assets/Scripts/Views/Collector.cs
+++ Assets/Scripts/Views/Collector.cs
@@ -1,9 +1,6 @@
 ﻿using Assets.Scripts.Data;
 using Assets.Scripts.Services.App;
-using Example;
 using Fusion;
-using System.Collections.Generic;
-using UnityEngine;
 using Zenject;
 
 namespace Assets.Scripts.Views
@@ -12,12 +9,7 @@ namespace Assets.Scripts.Views
     {
 
         [Inject] private readonly PlayerInventoryService playerInventory;
-
-        [SerializeField] private GameObject[] collectablePrefabs;
-        
-        private float spawnTimer = 0.0f;
-        private readonly float spawnCooldown = 1.0f;
-        
+                
         [Networked, Capacity(5)] public NetworkDictionary<CollectableType, int> Collected => default;
 
         public void EnqueueForCollection(CollectableType collectableType, int count)
@@ -25,23 +17,5 @@ namespace Assets.Scripts.Views
             playerInventory.AddCollectableItem(collectableType, count);
             Collected.Set(collectableType, playerInventory.Collectables[collectableType]);
         }
-
-        public override void FixedUpdateNetwork()
-        {
-            if (Runner.TryGetInputForPlayer(Object.InputAuthority, out GameplayInput input) && 
-                input.RMB && spawnTimer <= 0.0f &&
-                playerInventory.Collectables[CollectableType.Box] > 0)
-            {
-                spawnTimer = spawnCooldown;
-                Runner.Spawn(collectablePrefabs[(int)CollectableType.Box], transform.position + transform.forward * 2.0f);
-            }                
-        }
-
-        private void Update()
-        {
-            if (spawnTimer > 0.0f)
-                spawnTimer -= Time.deltaTime;
-        }
-
     }
 }
---
+++ Assets/Scripts/Views/Dropper.cs
@@ -0,0 +1,70 @@
+﻿using Assets.Scripts.Data;
+using Assets.Scripts.Services.App;
+using Example;
+using Fusion;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Views
+{
+    public class Dropper : NetworkBehaviour
+    {
+        [Inject] private readonly PlayerInventoryService playerInventory;
+
+        private readonly float coolDownTime = 1.0f;
+
+        [SerializeField] private GameObject[] collectablePrefabs;
+
+        [Networked] private TickTimer spawnTimer { get; set; }
+
+        private void InitSpawnTimer()
+        {
+            spawnTimer = TickTimer.CreateFromSeconds(Runner, coolDownTime);
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (spawnTimer.ExpiredOrNotRunning(Runner) &&
+                Runner.TryGetInputForPlayer(Object.InputAuthority, out GameplayInput input) &&
+                input.RMB &&
+                TryGetCollectableToDrop(out CollectableType collectableType))
+            {
+                InitSpawnTimer();
+                Runner.Spawn(collectablePrefabs[(int)collectableType], transform.position + transform.forward * 2.0f, Quaternion.identity, null, (runner, obj) => InitDroppedCollectable(runner, obj));
+                playerInventory.AddCollectableItem(collectableType, -1);
+            }
+        }
+
+        private bool TryGetCollectableToDrop(out CollectableType collectableType)
+        {
+            collectableType = default;
+
+            var available = playerInventory.Collectables.Where(c => c.Value > 0).Select(c => c.Key).ToArray();
+            if (available.Length > 0)
+            {
+                collectableType = available[Random.Range(0, available.Length)];
+                return true;
+            }
+
+            return false;
+        }
+
+        private void InitDroppedCollectable(NetworkRunner runner, NetworkObject obj)
+        {
+            List<CollectableSpawnPoint> spawnPoints =
+                runner.SimulationUnityScene.GetComponents<CollectableSpawnPoint>();
+            var closest = spawnPoints.OrderBy(s => Vector3.Distance(s.transform.position, transform.position)).FirstOrDefault();
+
+            var parentObj = closest.GetComponent<NetworkObject>();
+            var attachable = obj.GetComponent<AttachableView>();
+            attachable.InitForAnchorRef(parentObj.Id, false);
+
+            var despawnable = obj.GetComponent<Despawnable>();
+            despawnable.InitForLifeTime(Random.Range(5.0f, 15.0f));
+
+        }
+
+    }
+}
